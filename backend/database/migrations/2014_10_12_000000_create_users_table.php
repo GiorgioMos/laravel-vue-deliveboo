063c82fadffa7e0596bf853cf07400773ede0220
Eugen <epopescuu@yahoo.it>
2024-02-23 10:47:36 +0100

    refactor: refactoring admin dashboard

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('surname');
+            $table->string('surname')->nullable();
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('address');
-            $table->string('p_iva')->unique();
-            $table->string('telephone');
+            $table->string('address')->nullable();
+            $table->string('p_iva')->unique()->nullable();
+            $table->string('telephone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])
+    ->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
+    ->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
+    ->group(function () {
 
-require __DIR__.'/auth.php';
+        //Siamo nel gruppo quindi:
+        // - il percorso "/" diventa "admin/"
+        // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    });
+
+require __DIR__ . '/auth.php';
