aca85ba8666d18c0422b9ca5efb1a479c58ecb64
GiorgioMos <giorgiomosca2@gmail.com>
2024-02-27 19:00:42 +0100

    feat: add style checkbox in restaurant crud

--- backend/resources/views/admin/restaurants/create.blade.php
+++ backend/resources/views/admin/restaurants/create.blade.php
@@ -1,121 +1,141 @@
-@extends("layouts.admin")
+@extends('layouts.admin')
 
-@section("content")
-<div class="container py-3">
+<style>
+    @import "{{ asset('resources/scss/prova.css') }}";
+</style>
 
-    <div class="row">
-        <h1>Insert new restaurant</h1>
-        @isset($errore)
-        <div class="alert alert-danger">
-            <strong>{{$errore}}</strong>
+@section('content')
+    <div class="container py-3">
+
+        <div class="row">
+            <h1>Insert new restaurant</h1>
+            @isset($errore)
+                <div class="alert alert-danger">
+                    <strong>{{ $errore }}</strong>
+                </div>
+            @endisset
         </div>
-        @endisset
-    </div>
 
-    <div class="row">
-        <div class="col-6">
-            <form action="{{ route("admin.restaurants.store") }}" method="POST">
-                {{-- cross scripting request forgery --}}
-                @csrf
+        <div class="row">
+            <div class="col-6">
+                <form class="needs-validation" action="{{ route('admin.restaurants.store') }}" method="POST">
+                    {{-- cross scripting request forgery --}}
+                    @csrf
 
-                {{-- name  --}}
-                <div class="mb-3">
-                    <label for="name" class="form-label">Name</label>
-                    <input type="text" class="form-control @error(" name") is-invalid @enderror" id="name" name="name" value="{{ old("name") }}">
+                    {{-- name  --}}
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" placeholder="*" class="form-control @error('name') is-invalid @enderror"
+                            id="name" name="name" value="{{ old('name') }}">
 
-                    {{-- error message --}}
-                    @error("name")
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
+                        {{-- error message --}}
+                        @error('name')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                {{-- description  --}}
-                <div class="mb-3">
-                    <label for="description" class="form-label">description</label>
-                    <input type="text" class="form-control @error(" description") is-invalid @enderror" id="description" name="description" value="{{ old("description") }}">
+                    {{-- description  --}}
+                    <div class="mb-3">
+                        <label for="description" class="form-label">description</label>
+                        <input type="text" class="form-control @error('description') is-invalid @enderror"
+                            id="description" name="description" value="{{ old('description') }}">
 
-                    {{-- error message --}}
-                    @error("description")
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
+                        {{-- error message --}}
+                        @error('description')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                {{-- city  --}}
-                <div class="mb-3">
-                    <label for="city" class="form-label">city</label>
-                    <input type="text" class="form-control @error(" city") is-invalid @enderror" id="city" name="city" value="{{ old("city") }}">
+                    {{-- city  --}}
+                    <div class="mb-3">
+                        <label for="city" class="form-label">city</label>
+                        <input type="text" placeholder="*" class="form-control @error('city') is-invalid @enderror"
+                            id="city" name="city" value="{{ old('city') }}">
 
-                    {{-- error message --}}
-                    @error("city")
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
+                        {{-- error message --}}
+                        @error('city')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                {{-- address  --}}
-                <div class="mb-3">
-                    <label for="address" class="form-label">address</label>
-                    <input type="text" class="form-control @error(" address") is-invalid @enderror" id="address" name="address" value="{{ old("address") }}">
+                    {{-- address  --}}
+                    <div class="mb-3">
+                        <label for="address" class="form-label">address</label>
+                        <input type="text" placeholder="*" class="form-control @error('address') is-invalid @enderror"
+                            id="address" name="address" value="{{ old('address') }}">
 
-                    {{-- error message --}}
-                    @error("address")
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
+                        {{-- error message --}}
+                        @error('address')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                {{-- img  --}}
-                <div class="mb-3">
-                    <label for="img" class="form-label">img</label>
-                    <input type="text" class="form-control @error(" img") is-invalid @enderror" id="img" name="img" value="{{ old("img") }}">
+                    {{-- img  --}}
+                    <div class="mb-3">
+                        <label for="img" class="form-label">img</label>
+                        <input type="text" class="form-control @error('img') is-invalid @enderror" id="img"
+                            name="img" value="{{ old('img') }}">
 
-                    {{-- error message --}}
-                    @error("img")
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
+                        {{-- error message --}}
+                        @error('img')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                {{-- telephone  --}}
-                <div class="mb-3">
-                    <label for="telephone" class="form-label">telephone</label>
-                    <input type="text" class="form-control @error(" telephone") is-invalid @enderror" id="telephone" name="telephone" value="{{ old("telephone") }}">
+                    {{-- telephone  --}}
+                    <div class="mb-3">
+                        <label for="telephone" class="form-label">telephone</label>
+                        <input type="text" placeholder="*" class="form-control @error('telephone') is-invalid @enderror"
+                            id="telephone" name="telephone" value="{{ old('telephone') }}">
 
-                    {{-- error message --}}
-                    @error("telephone")
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
+                        {{-- error message --}}
+                        @error('telephone')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                {{-- website  --}}
-                <div class="mb-3">
-                    <label for="website" class="form-label">website</label>
-                    <input type="text" class="form-control @error(" website") is-invalid @enderror" id="website" name="website" value="{{ old("website") }}">
+                    {{-- website  --}}
+                    <div class="mb-3">
+                        <label for="website" class="form-label">website</label>
+                        <input type="text" placeholder="*" class="form-control @error('website') is-invalid @enderror"
+                            id="website" name="website" value="{{ old('website') }}">
 
-                    {{-- error message --}}
-                    @error("website")
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
+                        {{-- error message --}}
+                        @error('website')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                @isset($categories)
-                {{-- select category --}}
-                <div class="mb-3">
-                    <label for="categories" class="form-label">seleziona le categorie</label>
-                    <select multiple name="categories[]" id="categories" class="form-select">
-                        <option value="">seleziona almeno un category</option>
+                    <p>I campi con * sono obbligatori</p>
+
+                    @isset($categories)
                         @foreach ($categories as $category)
-                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            <div class="btn-group mb-3" role="group" aria-label="Basic checkbox toggle button group">
+                                <input hidden type="checkbox" name="categories[]" id="category{{ $category->id }}"
+                                    value="{{ $category->id }}" autocomplete="off">
+                                <label class="btn btn-outline-primary form-label rounded" for="category{{ $category->id }}">
+                                    {{ $category->name }}
+                                </label>
+                            </div>
                         @endforeach
-                    </select>
-                </div>
-                @endisset
+                    @endisset
 
+                    <br>
 
-                @if(isset($errore))
-                <button type="submit" class="btn btn-dark disabled">Create</button>
-                @else
-                <button type="submit" class="btn btn-dark">Create</button>
-                @endif
-            </form>
+                    @if (isset($errore))
+                        <button type="submit" class="btn btn-dark disabled">Create</button>
+                    @else
+                        <button type="submit" class="btn btn-dark">Create</button>
+                    @endif
+                </form>
+            </div>
         </div>
     </div>
-</div>
 @endsection
+
+<style>
+    input[type="checkbox"]:checked+label {
+        background-color: #007bff;
+        color: #fff;
+    }
+</style>
