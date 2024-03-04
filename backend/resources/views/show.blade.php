615e15735d3e578127ffe5cdb43e8ca26bdd7a82
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2024-03-04 18:28:16 +0100

    feat: quantity of selected products now shows on page

--- backend/resources/views/show.blade.php
+++ backend/resources/views/show.blade.php
@@ -26,9 +26,17 @@
                 <ul>
 
                     @foreach ($products as $product)
-                        <li>{{ $product->name }}</li>
+                        <li class="product" id="product-{{ $product->id }}">
+                            <button class="btn btn-primary"
+                                onclick="cartAddElement({{ $product->id }}, '{{ $product->name }}')">+</button>
+                            <span class="counter" id="{{ $product->id }}span">0</span>
+                            <button class="btn btn-danger"
+                                onclick="cartRemoveElement({{ $product->id }}, '{{ $product->name }}')">-</button>
+                            {{ $product->name }}
+                        </li>
                     @endforeach
                 </ul>
+
 
             </div>
             <div class="py-3 text-center">
@@ -36,4 +44,47 @@
             </div>
         </div>
     </div>
+    <script>
+        var productsQuantity = []
+
+        document.querySelectorAll('.product').forEach(element => {
+            let currentElement = {
+                id: element.id,
+                quantity: 0,
+            }
+            productsQuantity.push(currentElement);
+
+        });
+
+        console.log(productsQuantity);
+
+        function cartAddElement(product_id, product_name) {
+            let index = productsQuantity.findIndex(element => element.id === `product-` + product_id)
+
+
+            productsQuantity[index].quantity++;
+
+            console.log(index)
+            console.log(productsQuantity);
+
+            document.getElementById(product_id + 'span').innerHTML = productsQuantity[index].quantity;
+
+        }
+
+        function cartRemoveElement(product_id, product_name) {
+            let index = productsQuantity.findIndex(element => element.id === `product-` + product_id)
+
+
+            if (productsQuantity[index].quantity > 0) {
+                productsQuantity[index].quantity--;
+            } else {
+                productsQuantity[index].quantity = 0;
+            }
+
+            console.log(index)
+            console.log(productsQuantity)
+            document.getElementById(product_id + 'span').innerHTML = productsQuantity[index].quantity;
+
+        }
+    </script>
 @endsection
