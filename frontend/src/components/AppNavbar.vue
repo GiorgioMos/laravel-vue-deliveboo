2fb26fe09bf60bff244bb78121d4083b7dff28d6
Marco <marcoregina92@gmail.com>
2024-03-07 11:18:30 +0100

    feat: Add logo deliveboo

--- frontend/src/components/AppNavbar.vue
+++ frontend/src/components/AppNavbar.vue
@@ -1,13 +1,13 @@
 <script>
-import  functions  from '../functions.js'
+import functions from '../functions.js'
 
 export default {
   name: "AppNavbar",
-  created() { 
-      this.riempiCarrello = functions.riempiCarrello // recupera funzione in gunction.js
-      this.clearCart = functions.clearCart // recupera funzione in gunction.js
+  created() {
+    this.riempiCarrello = functions.riempiCarrello // recupera funzione in gunction.js
+    this.clearCart = functions.clearCart // recupera funzione in gunction.js
 
-    },
+  },
   data() {
     return {
       menuItems: [
@@ -47,19 +47,21 @@ export default {
 <!-- logo a sinistra pagine al centro che riportano ai componenti e a destra carrello lohgin e registrazione -->
 <template>
   <nav class="navbar navbar-expand-lg">
-  <div class="container">
-      <a class="navbar-brand text-light" href="/">LOGO</a>
+    <div class="container">
+    <a class="navbar-brand text-light" href="/">
+      <img class="logoDeliveboo" src="src/assets/img/logoDeliveboo.png" alt="logoDeliveboo">
+    </a>
 
     <ul class="navbar-nav">
-      <li class="nav-item">
+        <li class="nav-item">
         <a class="page-navigation" href="#">Ristoranti</a>
       </li>
       <li class="nav-item">
-          <a class="page-navigation" href="/about">Chi Siamo</a>
-      </li>
-      <li class="nav-item">
-        <a class="page-navigation" href="#">Prodotti</a>
-      </li>
+        <a class="page-navigation" href="/about">Chi Siamo</a>
+        </li>
+        <li class="nav-item">
+          <a class="page-navigation" href="#">Prodotti</a>
+        </li>
       </ul>
 
       <ul class="navbar-nav">
@@ -67,7 +69,7 @@ export default {
         <!-- bottone carrello offcanvas -->
         <li class="nav-item">
           <a class="btn rounded-pill btn-outline-light px-4" href="#" id="shopping-cart" data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+            data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
             <font-awesome-icon icon="fa-solid fa-cart-shopping" />
           </a>
         </li>
@@ -82,45 +84,49 @@ export default {
   </nav>
 
   <!--   <nav class="navbar border-body">
-          <div class="container">
-            <a class="navbar-brand text-light" href=".">Logo</a>
-          </div>
-          <div>
-            <a href="" class="navbar-text">sdfg</a>
-            <a href="" class="nav-link">asdf</a>
-            <a href="" class="nav-link">asdf</a>
-          </div>
-        </nav> -->
+                        <div class="container">
+                          <a class="navbar-brand text-light" href=".">Logo</a>
+                        </div>
+                        <div>
+                          <a href="" class="navbar-text">sdfg</a>
+                          <a href="" class="nav-link">asdf</a>
+                          <a href="" class="nav-link">asdf</a>
+                        </div>
+                      </nav> -->
 
   <!-- <div class="col-3 text-start">
-      						<a href=".">
-      							<link rel="stylesheet" href="/public/router.png">
-      							<img src="/public/router.png" alt="">
-      						</a>
-      					</div> -->
+                    						<a href=".">
+                    							<link rel="stylesheet" href="/public/router.png">
+                    							<img src="/public/router.png" alt="">
+                    						</a>
+                    					</div> -->
   <!-- <div v-for="(item, index) in menuItems" :key="index" class="col-2 d-flex justify-content-center align-items-center">
-      						<router-link :to="{name: item.routeName}" class="nav-link">
-      							<h3>{{ item.label }}</h3>
-      						</router-link>
-      					</div> -->
+                    						<router-link :to="{name: item.routeName}" class="nav-link">
+                    							<h3>{{ item.label }}</h3>
+                    						</router-link>
+                    					</div> -->
 
-                <!-- OFFCANVAS -->
-                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample"
-            aria-labelledby="offcanvasExampleLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Carrello</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <div id="offcanvas-body">
+  <!-- OFFCANVAS -->
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasExampleLabel">Carrello</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <div id="offcanvas-body">
 
-                </div>
-                <button id="clearCart" class="btn btn-primary" @click="this.clearCart(); this.riempiCarrello()"> Svuota carrello</button>
-            </div>
-        </div>
+      </div>
+      <button id="clearCart" class="btn btn-primary" @click="this.clearCart(); this.riempiCarrello()"> Svuota
+        carrello</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.logoDeliveboo {
+  width: 8rem;
+}
+
 .page-navigation {
   color: white;
   text-decoration: none;
