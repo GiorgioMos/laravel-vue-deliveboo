82fb8cd17a0f2917cdd47210ba9fef5fd28585a7
mazzocchifrancesco <123@email.it>
2024-03-11 16:02:20 +0100

    feat: add restaurant and products to mail

--- backend/app/Http/Controllers/Api/OrderController.php
+++ backend/app/Http/Controllers/Api/OrderController.php
@@ -15,19 +15,23 @@ class OrderController extends Controller
     {
         $validated = $request->validated();
         $newOrder = new Order();
-        $dataMail = new Order();
-        $dataMail->fill($validated);
-
         $newOrder->fill($validated);
-
-        $email = $validated["guest_name"] . "." . $validated["guest_surname"] . "@gmail.com";
         $newOrder->save();
-        Mail::to($email)->send(new NewContact($dataMail));
 
         // mi arriva nella request un array di oggetti key value con id e quantità, lo chiamo PRODUCTS
         foreach ($request->products as $product) {
             $newOrder->products()->attach($product['id'], ['quantity' => $product['quantity']]);
         }
+
+        // creo un oggetto con tutti i dati che mi serviranno nella mail 
+        $dataMail = new Order();
+        $dataMail->fill($validated);
+        $dataMail['products'] = $validated['products'];
+        // costruisco l'indirizzo email 
+        $email = $validated["guest_name"] . "." . $validated["guest_surname"] . "@gmail.com";
+        // passo i dati alla vista e invio la mail 
+        Mail::to($email)->send(new NewContact($dataMail));
+
         return response()->json(['messaggio' => 'Dati salvati con successo'], 200);
     }
 }
--- backend/resources/views/emails/new-contact-mail.blade.php
+++ backend/resources/views/emails/new-contact-mail.blade.php
@@ -1,17 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+@php
+    use App\Models\Product;
+    use App\Models\Restaurant;
+
+    $restaurant_id = Product::select('restaurant_id')
+        ->where('id', $dataMail->products[0]['id'])
+        ->first();
+    $restaurant = Restaurant::select('name')
+        ->where('id', $restaurant_id->restaurant_id)
+        ->first();
+    $products = Product::all();
+
+    function getProduct($id)
+    {
+        return Product::where('id', $id)->first();
+    }
+@endphp
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <h1 class="text-center mb-5">Grazie per il tuo Odine, {{ $dataMail->guest_name }}</h1>
     <h4>Ecco i dettagli del tuo Ordine, effettuato in data <span>{{ $dataMail->date }}</span>:</h4>
 
+    <h3 class="my-4">Hai ordinato da <span class="fw-bolder">{{ $restaurant->name }}</span></h3>
+    <p>Importo totale: <strong>{{ $dataMail->total_price }} € </strong></p>
+    <p>Note: <strong>{{ $dataMail->notes }}</strong></p>
+    <p>Prodotti acquistati:</p>
+    @foreach ($dataMail->products as $prodotto)
+        <div>
+            <span class="fw-bolder">{{ getProduct($prodotto['id'])->name }}</span> --> <span
+                class="fw-bolder">{{ $prodotto['quantity'] }}</span><span>(
+                {{ getProduct($prodotto['id'])->price * $prodotto['quantity'] }} €)</span>
+        </div>
+    @endforeach
     <h3 class="my-4">Dati Personali:</h3>
     <p>Nome: <strong>{{ $dataMail->guest_name }}</strong></p>
     <p>Cognome: <strong>{{ $dataMail->guest_surname }}</strong></p>
@@ -20,9 +50,6 @@
     <h3 class="my-4">Dati per la consegna:</h3>
     <p>Indirizzo: <strong>{{ $dataMail->guest_address }}</strong></p>
     <p>Città: <strong>{{ $dataMail->guest_city }}</strong></p>
-    <h3 class="my-4">Informazioni ordine:</h3>
-    <p>Importo totale: <strong>{{ $dataMail->total_price }} € </strong></p>
-    <p>Note: <strong>{{ $dataMail->notes }}</strong></p>
 
 
 
@@ -34,6 +61,9 @@
 
 
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
